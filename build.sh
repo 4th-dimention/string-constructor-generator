166cf4e8c48acebbe502a2b454990fe6aab64b9a
Allen Webster <yoyo4thdimention@gmail.com>
2019-08-31 00:55:18 -0700

    Fixup build script paths

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-code="$PWD"
-opts=-g
-cd ../build > /dev/null
-g++ $opts $code/main.cpp -o model
-cd $code > /dev/null
--- build_generator.bat
+++ build_generator.bat
@@ -2,6 +2,9 @@
 
 set opts=-FC -GR- -EHa- -nologo -Zi
 set code=%cd%
+
+IF NOT EXIST "%code%\..\build" (mkdir %code%\..\build)
+
 pushd ..\build
 cl %opts% %code%\string_constructor_generator.cpp -Fegenerator
 popd
